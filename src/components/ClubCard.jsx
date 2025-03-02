ff76534d7f34a32652637f66805e2ff03c7c9b5f
prdcr-gem <deemaine628@gmail.com>
2025-03-02 02:59:49 -0500

    Updated jsx files and added css files for the components

--- src/components/ClubCard.jsx
+++ src/components/ClubCard.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import SchoolOrgs from "./SchoolOrgs";
+import "./ClubCard.css";
 
 // props is a way that passes data from one component to another component.
-const ClubCard = (props) => {
+const ClubCard = ({orgName, image, desc}) => {
     return (
-       <td>{props.orgName}</td>
+       <div className="club-card">
+            <img src={image} alt={orgName} className="club-image" />
+            <h2>{orgName}</h2>
+            <p>{desc}</p>
+            <button className="club-button"> Learn More </button>
+
+       </div>
+       
     )
 }
 
