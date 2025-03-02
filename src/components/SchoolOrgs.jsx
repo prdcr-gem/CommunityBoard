4c00efecfd224e68c107bdbf2ee51321db1f8fdc
prdcr-gem <deemaine628@gmail.com>
2025-03-02 04:01:33 -0500

    adding pics and making cards

--- src/components/SchoolOrgs.jsx
+++ src/components/SchoolOrgs.jsx
@@ -8,11 +8,11 @@ const SchoolOrgs = () => {
         <div className= "school-orgs">
             <h1> Favorite Affliated & Non-Affliated School Organizations:</h1>
             <div className="club-container">
-                <ClubCard orgName ="WiCyS UCF" desc="Women in Cybersecurity at UCF" image="" />
-                <ClubCard orgName ="Hack UCF" desc="Cybersecurity focused on ethical hacking" image=""/>
-                <ClubCard orgName ="CodePath" desc ="A non-profit organization helps underrespresented students" image ="" />
-                <ClubCard orgName ="Design & Code Club" desc="Where art meets code" img="" />
-                <ClubCard orgName ="Art with Flo" desc="A community where welcomes all to the world of digital art" image="" />
+                <ClubCard orgName ="WiCyS UCF" desc="Women in Cybersecurity at UCF" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9pOUUo2UevcrNrFHsXEKKMUDOzomk7dmb1vvfpFJQjJgvpkSKu_Xp8b2r6GJJjGTb0GQ&usqp=CAU" />
+                <ClubCard orgName ="Hack UCF" desc="Cybersecurity focused on ethical hacking" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVVi0rQpYdCUHnXoA1NsytxjxtXQ8XdFmAjQ&s"/>
+                <ClubCard orgName ="CodePath" desc ="A non-profit organization helps underrespresented students" image ="https://mma.prnewswire.com/media/1800392/codepath_1x1_solid_dark_Logo.jpg?p=publish" />
+                <ClubCard orgName ="Design & Code Club" desc="Where art meets code" img="src/pics/DesignCodeTriLogo-12f8a23e.png" />
+                <ClubCard orgName ="Art with Flo" desc="A community where welcomes all to the world of digital art" image="https://ci3.googleusercontent.com/meips/ADKq_NablYqjKnkakgqgScmAuA0dctCRIVgNfCdAd1B10lQUaF2S4eRKrliGFS4Xe4wxUKJCLnGzkTp9L9Ct5DNU0w9_rp2Vb1TWp5AnSoEw6FkcAetjHnFPRqTcSsf58Od_SdU7S6IcUVyfqlWWEtRym_lA5pKe5R-38dPlEtHX6MrnrbMy7ncEcvGVOuskLp8DifU9ghOK5a_NVTnFwYGi2yXubA=s0-d-e1-ft#https://usercontent.flodesk.com/512b0ff7-bc05-427b-acf4-a51c7794fdae/upload/ff08e8ab-7610-41c0-bece-68aa5cf240d1.png?width=2048&height=2048" />
                 <ClubCard orgName ="Animation Production Club " desc="An animation community at UCF" img="" />
                 <ClubCard orgName ="Spacedad Space Club" desc="A global animation community focuses on leveraging and enhancing your animation skills" image=""/>
                 <ClubCard orgName ="Society of PC Building at UCF" desc="An indepth club course for PC Building at UCF" img=""/>
