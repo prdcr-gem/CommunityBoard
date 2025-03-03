1eaa710e44526faf3949061476a2fb7d3324c8e7
prdcr-gem <deemaine628@gmail.com>
2025-03-03 02:49:18 -0500

    added a link

--- src/components/ClubCard.jsx
+++ src/components/ClubCard.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./ClubCard.css";
 
 // props is a way that passes data from one component to another component.
-const ClubCard = ({orgName, image, desc}) => {
+const ClubCard = ({orgName, image, desc, link}) => {
     return (
        <div className="club-card">
             <img src={image} alt={orgName} className="club-image" />
             <h2>{orgName}</h2>
             <p>{desc}</p>
             <button className="club-button"> Learn More </button>
+
+            {/* _blank prompts to open the link in a new tab */}
+            <a href={link} target="_blank" rel="noopener noreferrer" />
+                <button className="club-button">Learn More</button>
 
        </div>
        
--- src/components/SchoolOrgs.jsx
+++ src/components/SchoolOrgs.jsx
@@ -7,19 +7,42 @@ const SchoolOrgs = () => {
 
     return (
         <div className= "school-orgs">
-            <h1> Favorite Affliated & Non-Affliated School Organizations:</h1>
+            <h1> Top Favorite School Organizations: Art, Cybersecurity, and Women in Tech</h1>
             <div className="club-container">
-                <ClubCard orgName ="WiCyS UCF" desc="Women in Cybersecurity at UCF" image ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9pOUUo2UevcrNrFHsXEKKMUDOzomk7dmb1vvfpFJQjJgvpkSKu_Xp8b2r6GJJjGTb0GQ&usqp=CAU" />
-                <ClubCard orgName ="Hack UCF" desc="Cybersecurity focused on ethical hacking" image ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVVi0rQpYdCUHnXoA1NsytxjxtXQ8XdFmAjQ&s"/>
-                <ClubCard orgName ="CodePath" desc ="A non-profit organization helps underrespresented students" image ="https://mma.prnewswire.com/media/1800392/codepath_1x1_solid_dark_Logo.jpg?p=publish" />
-                <ClubCard orgName ="Design & Code Club" desc="Where art meets code" image="https://designandcodeucf.com/assets/DesignCodeTriLogo-12f8a23e.png" />
-                <ClubCard orgName ="Art with Flo" desc="A community where welcomes all to the world of digital art" image ="https://ci3.googleusercontent.com/meips/ADKq_NablYqjKnkakgqgScmAuA0dctCRIVgNfCdAd1B10lQUaF2S4eRKrliGFS4Xe4wxUKJCLnGzkTp9L9Ct5DNU0w9_rp2Vb1TWp5AnSoEw6FkcAetjHnFPRqTcSsf58Od_SdU7S6IcUVyfqlWWEtRym_lA5pKe5R-38dPlEtHX6MrnrbMy7ncEcvGVOuskLp8DifU9ghOK5a_NVTnFwYGi2yXubA=s0-d-e1-ft#https://usercontent.flodesk.com/512b0ff7-bc05-427b-acf4-a51c7794fdae/upload/ff08e8ab-7610-41c0-bece-68aa5cf240d1.png?width=2048&height=2048" />
-                <ClubCard orgName ="Animation Production Club " desc="An animation community at UCF" image ="https://se-images.campuslabs.com/clink/images/9c4bd7d2-df9e-4de6-8dd6-dc3613a8f1a6c20e5ec9-feaa-4d6b-9a76-38fd478b5915.png?preset=med-sq" />
-                <ClubCard orgName ="Spacedad Space Club" desc="A global animation community focuses on leveraging and enhancing your animation skills" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyQCQ5kOpB3p_JwVWHd-UyfikKW5f8vnW5IA&s"/>
-                <ClubCard orgName ="Society of PC Building at UCF" desc="An indepth club course for PC Building at UCF" image ="https://se-images.campuslabs.com/clink/images/99c0f67b-b28c-4237-a943-90078d7f4ee46634f305-21e7-4aa4-bc5b-1b937dc452a6.PNG?preset=med-sq"/>
-                <ClubCard orgName ="Bsides" desc="An affliated cybersecurity community" image ="https://bsidesorlando.org/assets/images/logo-b.png" />
-                <ClubCard orgName ="SWE UCF" desc="Society of Women Engineers at UCF" image ="https://se-images.campuslabs.com/clink/images/27c36c54-29e2-4107-836c-aba6355c0588615f270d-644d-4d2c-9135-89fc3cda6725.png?preset=med-sq" />
-                <ClubCard orgName ="WEEC UCF" desc="Women in Electrical Engineers and Computer Science at UCF" image ="https://se-images.campuslabs.com/clink/images/62c4513a-992f-481b-bec2-394ea76b6f11cabcbce2-f5e0-44f8-8fa9-e796ad78db60.jpg?preset=med-sq" />
+                <ClubCard orgName ="WiCyS UCF" 
+                          desc="Women in Cybersecurity at UCF"
+                          image ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9pOUUo2UevcrNrFHsXEKKMUDOzomk7dmb1vvfpFJQjJgvpkSKu_Xp8b2r6GJJjGTb0GQ&usqp=CAU"
+                          link= "https://hackucf.org/wicys" />
+                <ClubCard orgName ="Hack UCF" 
+                          desc="Cybersecurity focused on ethical hacking" 
+                          image ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVVi0rQpYdCUHnXoA1NsytxjxtXQ8XdFmAjQ&s"/>
+                <ClubCard orgName ="CodePath" 
+                          desc ="A non-profit organization helps underrespresented students" 
+                          image ="https://mma.prnewswire.com/media/1800392/codepath_1x1_solid_dark_Logo.jpg?p=publish" />
+                <ClubCard orgName ="Design & Code Club" 
+                          desc="Where art meets code" 
+                          image="https://designandcodeucf.com/assets/DesignCodeTriLogo-12f8a23e.png" />
+                <ClubCard orgName ="Art with Flo" 
+                          desc="A community where welcomes all to the world of digital art" 
+                          image ="https://ci3.googleusercontent.com/meips/ADKq_NablYqjKnkakgqgScmAuA0dctCRIVgNfCdAd1B10lQUaF2S4eRKrliGFS4Xe4wxUKJCLnGzkTp9L9Ct5DNU0w9_rp2Vb1TWp5AnSoEw6FkcAetjHnFPRqTcSsf58Od_SdU7S6IcUVyfqlWWEtRym_lA5pKe5R-38dPlEtHX6MrnrbMy7ncEcvGVOuskLp8DifU9ghOK5a_NVTnFwYGi2yXubA=s0-d-e1-ft#https://usercontent.flodesk.com/512b0ff7-bc05-427b-acf4-a51c7794fdae/upload/ff08e8ab-7610-41c0-bece-68aa5cf240d1.png?width=2048&height=2048" />
+                <ClubCard orgName ="Animation Production Club " 
+                          desc="An animation community at UCF" 
+                          image ="https://se-images.campuslabs.com/clink/images/9c4bd7d2-df9e-4de6-8dd6-dc3613a8f1a6c20e5ec9-feaa-4d6b-9a76-38fd478b5915.png?preset=med-sq" />
+                <ClubCard orgName ="Spacedad Space Club" 
+                          desc="A global animation community focuses on leveraging and enhancing your animation skills" 
+                          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyQCQ5kOpB3p_JwVWHd-UyfikKW5f8vnW5IA&s"/>
+                <ClubCard orgName ="Society of PC Building at UCF" 
+                          desc="An indepth club course for PC Building at UCF" 
+                          image ="https://se-images.campuslabs.com/clink/images/99c0f67b-b28c-4237-a943-90078d7f4ee46634f305-21e7-4aa4-bc5b-1b937dc452a6.PNG?preset=med-sq"/>
+                <ClubCard orgName ="Bsides" 
+                          desc="An affliated cybersecurity community" 
+                          image ="https://bsidesorlando.org/assets/images/logo-b.png" />
+                <ClubCard orgName ="SWE UCF" 
+                          desc="Society of Women Engineers at UCF" 
+                          image ="https://se-images.campuslabs.com/clink/images/27c36c54-29e2-4107-836c-aba6355c0588615f270d-644d-4d2c-9135-89fc3cda6725.png?preset=med-sq" />
+                <ClubCard orgName ="WEEC UCF" 
+                          desc="Women in Electrical Engineers and Computer Science at UCF" 
+                          image ="https://se-images.campuslabs.com/clink/images/62c4513a-992f-481b-bec2-394ea76b6f11cabcbce2-f5e0-44f8-8fa9-e796ad78db60.jpg?preset=med-sq" />
             </div>
             
 
